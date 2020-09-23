53295be0cd6f7c9eecc5f0a7708b0a30d620a039
kmuguruma <koki_muguruma@forcia.com>
2020-09-23 20:04:08 +0900

    commit idを表示する

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 import (
+    "os/exec"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
-    data := "Hello Go!!"
+    out, _ := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
+    data := "Hello Go!!" + string(out)
     router.GET("/", func(ctx *gin.Context){
         ctx.HTML(200, "index.html", gin.H{"data": data})
     })
