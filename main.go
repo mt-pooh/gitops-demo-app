802116c9e1bc2b435d8e4f8363f602cb1b3133d3
kmuguruma <koki_muguruma@forcia.com>
2020-09-29 18:14:46 +0900

    change name

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func getPort() string {
 }
 
 func main() {
-   welcome := Welcome{"KOKIA", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
+   welcome := Welcome{"ArgoCD GitOps", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
 
    templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
