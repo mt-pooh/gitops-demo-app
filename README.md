415e7300a98ac1c2fef78cf93ca670cf2eaa5ec6
koki_muguruma <flash03stationery+git@gmail.com>
2020-10-26 04:47:24 +0900

    welcome to github actions

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ![CI status](https://github.com/forcia/gitops-demo-app/workflows/CI/badge.svg)
 # gitops-demo-app (Go)
+# CI
 Docker image push先(masterブランチにpushされるとCIで以下レポジトリにpushされます)
 https://hub.docker.com/repository/docker/musyu/gitops-demo-app
 
--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func getPort() string {
 }
 
 func main() {
-   welcome := Welcome{"Koki Muguruma", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
+   welcome := Welcome{"Github Actions", time.Now().Format(time.Stamp), os.Getenv("HOSTNAME")}
 
    templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
